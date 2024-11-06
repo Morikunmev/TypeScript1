f6fb8699b08bf6f704d094172ebe6ee9b1fa65a2
Richard <ricky201325@gmail.com>
2024-11-06 13:52:40 -0300

    termino de configuraciones

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
 let mensaje = "Hola mundo";
+mensaje = "teclado";
+console.log(mensaje);
+mensaje = "chao mundo";
+console.log(mensaje);
+console.log(typeof []);
+let extincionDinosaurios = 76000000;
+let dinosaourioFavorito = "T-Rex";
+let extintos = true;
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 let mensaje: string = "Hola mundo";
+mensaje = "teclado";
+
+console.log(mensaje);
+mensaje = "chao mundo";
+console.log(mensaje);
+console.log(typeof []);
+
+let extincionDinosaurios: number = 76_000_000;
+let dinosaourioFavorito: string = "T-Rex";
+let extintos: boolean = true;
+
+function chanchitoFeliz(config: any) {
+  return config;
+}
